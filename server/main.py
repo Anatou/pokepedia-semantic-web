bdb4d9e0dc1349bf8b961ec9029d7e8f8db8344d
Anatou <anatole@desnot.com>
2026-01-05 13:52:36 +0100

    feat: chargement, query simple, et base fastAPI

---
+++ server/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+# uvicorn main:app --reload
+
+# Create FastAPI app instance
+app = FastAPI()
+
+# Define a simple GET endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
