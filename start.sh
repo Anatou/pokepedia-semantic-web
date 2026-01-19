524a6da4382433b334dd88edffae5ac2d854f38d
Anatou <anatole@desnot.com>
2026-01-19 11:51:03 +0100

    fix: fixed for mac

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
-# Start SPARQL endpoint
-$TERMINAL --detach sh -c "cd server && uvicorn app:app --port 8010"
-
-# Start backend
-$TERMINAL --detach sh -c "cd client/back && uvicorn app:app --port 8020"
-
-# Start frontend
-$TERMINAL --detach sh -c "cd client/front && npm i && npm run dev"
+if [ "$OSTYPE" == "linux-gnu" ]; then
+    case "$TERMINAL" in
+        kitty)
+            # Start SPARQL endpoint
+            kitty --detach sh -c "cd server && uvicorn app:app --port 8010"
+            # Start backend
+            kitty --detach sh -c "cd client/back && uvicorn app:app --port 8020"
+            # Start frontend
+            kitty --detach sh -c "cd client/front && npm i && npm run dev";;
+        *) echo "Script not configured to open this terminal emulator"
+    esac 
+elif [ "$OSTYPE" == "darwin"* ]; then
+    osascript -e 'tell app "Terminal" to do script "cd server && uvicorn app:app --port 8010"'
+    osascript -e 'tell app "Terminal" to do script "cd client/back && uvicorn app:app --port 8020"'
+    osascript -e 'tell app "Terminal" to do script "cd client/front && npm i && npm run dev"'
+fi
