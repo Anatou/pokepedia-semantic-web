fc9c8309c0825cea5fc5856f2c6f2eb5702a4ef9
Anatou <anatole@desnot.com>
2026-01-19 11:22:58 +0100

    fix: fixed start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
 :: Start SPARQL endpoint
-start cmd /k cd server && uvicorn app:app --port 8010
+start cmd /k "cd server && python -m uvicorn app:app --port 8010"
 
 :: Start backend
-start cmd /k cd client/back && uvicorn app:app --port 8020
+start cmd /k "cd client/back && python -m uvicorn app:app --port 8020"
 
 :: Start frontend
-start cmd /k cd client/front && npm i && npm run dev
+start cmd /k "cd client/front && npm i && npm run dev"
